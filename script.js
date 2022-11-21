33ebc9720533542de75d71b5992c6d1a78df02b2
AFKMDaniel <danich734@gmail.com>
2022-11-21 23:53:32 +1000

    added block with fixed width and height

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const screenSpan = document.querySelector('.screen span');
 //-------------------------------------------------
 // В переменную ниже поместите нужный текст
 const text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore repellendus tempora quia facere aliquam inventore provident enim cum distinctio sunt ullam, ipsam voluptatibus dicta repellat tenetur minus ab ea commodi fugit quisquam at corporis exercitationem, dolorem rerum. Quia dolorem saepe modi, quam harum soluta maiores adipisci nobis ullam sint id, assumenda repellat ea? Perspiciatis corporis voluptatum optio, blanditiis, porro reiciendis omnis temporibus neque esse nulla ducimus, inventore saepe. Doloremque nesciunt praesentium consequatur quos est ex odit eum laudantium nisi illum maxime, nobis fugiat, architecto id exercitationem vel autem voluptates, dolorum omnis! Repellendus culpa voluptate iusto rem sed? Ipsa omnis, rerum excepturi ut dolores non cumque reiciendis sapiente nam aperiam architecto provident a error voluptas labore consequatur. Quaerat at consequuntur alias?';
-const typingSpeed = 0.075; // время появления одного символа в секундах
+const typingSpeed = 0.025; // время появления одного символа в секундах
 const needToClear = true; // нужно ли очищать экран
 const clearDelay = 0; // задержка очистки в секундах
 //-------------------------------------------------
